73e6de4bc6f027ec58c66c6d0eb4e11c107133f2
Isht <ae.isht@gmail.com>
2020-02-19 21:24:28 +0700

    edit

--- application/modules/catalogs/views/categories/admin/add.php
+++ application/modules/catalogs/views/categories/admin/add.php
@@ -1,23 +1,24 @@
 <!-- Toolbars -->
-<section class="content-header">
-    <a class="btn btn-sm btn-default btn-flat" href="<?php echo base_url('admin/catalogs'); ?>"><i class="fa fa-list"></i>&nbsp;&nbsp;Catalogs</a>
-    <a class="btn btn-sm btn-default btn-flat" style="margin-right: 5px;" href="<?php echo base_url('admin/catalogs/categories'); ?>"><i class="fa fa-list"></i>&nbsp;&nbsp;Categories&nbsp;&nbsp;&nbsp;<span class="label label-success"><?php echo $count_data; ?></span></a>
-</section>
+<h2 class="section-title">Add New Category</h2>
+<div class="row">
+    <div class="col-12 col-md-12 col-lg-12">
+        <a class="btn btn-sm btn-info" href="<?php echo base_url('admin/catalogs'); ?>"><i class="fa fa-list"></i>&nbsp;&nbsp;Catalogs</a>
+        <a class="btn btn-sm btn-info" style="margin-right: 5px;" href="<?php echo base_url('admin/catalogs/categories'); ?>"><i class="fa fa-list"></i>&nbsp;&nbsp;Categories&nbsp;&nbsp;&nbsp;<span class="badge badge-primary"><?php echo $count_data; ?></span></a>
+    </div>
+</div>
+</br>
 
 <!-- Main content -->
-<section class="content">
-    <!-- Default box -->
-    <div class="box box-success">
-        <div class="box-header with-border">
-            <h3 class="box-title">Add New Category</h3>
-        </div>
-        <div class="box-body">
-            <?php
-            if (!empty($msg)) {
-                echo $msg;
-            }
-            ?>
-            <?php echo form_open(uri_string()); ?>
+<div class="row">
+    <div class="col-12 col-md-12 col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <?php
+                if (!empty($msg)) {
+                    echo $msg;
+                }
+                ?>
+                <?php echo form_open(uri_string()); ?>
             <div class="form-group">
                 <label for="ct_name" class="control-label">Category Name</label>
                 <?php echo form_input($ct_name); ?>
@@ -30,11 +31,11 @@
                 <label for="ct_desc" class="control-label">Description</label>
                 <?php echo form_textarea($ct_desc); ?>
             </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-sm btn-flat btn-primary"><i class="fa fa-save"></i> Create</button>
-                <a class="btn btn-sm btn-flat btn-warning" style="margin-left: 5px;" href="<?php echo base_url('admin/catalogs/categories'); ?>"><i class="fa fa-rotate-left"></i> Batal</a>
-            </div>
-            <?php echo form_close(); ?>
-        </div><!-- /.box-body -->
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Create</button>
+                <a class="btn btn-warning" style="margin-left: 5px;" href="<?php echo base_url('admin/catalogs/categories'); ?>"><i class="fa fa-undo"></i> Batal</a>
+            </div> <!--/.box-footer-->
+        <?php echo form_close(); ?>
+        </div><!-- /.box -->
     </div><!-- /.box -->
-</section><!-- /.content -->
+</div><!-- /.content -->
